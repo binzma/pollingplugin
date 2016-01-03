0682672f2b4a9d20990c38f3641665651c4c6665
mba@gmx.ch <Drag0ner>
2016-01-03 18:57:12 +0100

    ASDF

--- src/android/ch/maenu/pollingplugin/PollingReceiver.java
+++ src/android/ch/maenu/pollingplugin/PollingReceiver.java
@@ -1,12 +1,8 @@
 package ch.maenu.pollingplugin;
 
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
 import android.os.Vibrator;
 import android.util.Log;
 
@@ -17,13 +13,13 @@ public class PollingReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d("PollingPlugin", "PollingEventReceived");
 
-        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-        WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-        wakeLock.acquire();
-
-        KeyguardManager keyguardManager = (KeyguardManager)context.getSystemService(Context.KEYGUARD_SERVICE);
-        KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
-        keyguardLock.disableKeyguard();
+//        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+//        WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+//        wakeLock.acquire();
+//
+//        KeyguardManager keyguardManager = (KeyguardManager)context.getSystemService(Context.KEYGUARD_SERVICE);
+//        KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
+//        keyguardLock.disableKeyguard();
 
 
         // TODO: remove vibrator for production
@@ -37,6 +33,7 @@ public class PollingReceiver extends BroadcastReceiver {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
+
 
 //    private void performAction(Context context){
 //
