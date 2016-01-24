325acbce9fded766ef3d7bc38ca1a4f9ddcf2a6d
mba@gmx.ch <Drag0ner>
2016-01-24 18:07:04 +0100

    notification now works

--- src/android/ch/maenu/pollingplugin/PollingPlugin.java
+++ src/android/ch/maenu/pollingplugin/PollingPlugin.java
@@ -1,14 +1,10 @@
 package ch.maenu.pollingplugin;
 
 import android.app.AlarmManager;
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -21,156 +17,145 @@ import org.json.JSONException;
 
 public class PollingPlugin extends CordovaPlugin {
 
-    /**
-     * Interval may not be smaller than 5 minutes
-     */
-    private final long MIN_INTERVAL = 5 * 60 * 1000;
 
+    @Override
+    public void onPause(boolean multitasking) {
+        Log.d("PollingPlugin", "onPause");
+        super.onPause(multitasking);
+    }
 
-	@Override
-	public void onPause(boolean multitasking) {
-		Log.d("PollingPlugin", "onPause");
-		super.onPause(multitasking);
-	}
+    @Override
+    public void onResume(boolean multitasking) {
+        Log.d("PollingPlugin", "onResume " );
+        super.onResume(multitasking);
 
-	@Override
-	public void onResume(boolean multitasking) {
-		Log.d("PollingPlugin", "onResume " );
-		super.onResume(multitasking);
+//        PowerManager pm = (PowerManager)this.cordova.getActivity().getSystemService(Context.POWER_SERVICE);
+//        WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+//        wakeLock.acquire();
+//
+//        KeyguardManager keyguardManager = (KeyguardManager) this.cordova.getActivity().getSystemService(Context.KEYGUARD_SERVICE);
+//        KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
+//        keyguardLock.disableKeyguard();
+    }
 
-		PowerManager pm = (PowerManager)this.cordova.getActivity().getSystemService(Context.POWER_SERVICE);
-		WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-		wakeLock.acquire();
+    @Override
+    public boolean execute(final String action, final JSONArray args, final CallbackContext command) {
 
-		KeyguardManager keyguardManager = (KeyguardManager) this.cordova.getActivity().getSystemService(Context.KEYGUARD_SERVICE);
-		KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
-		keyguardLock.disableKeyguard();
-	}
-
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-		try {
-
-
-            if ("addUrl".equals(action)) {
-
-                String url = args.getString(0);
-
-                // TODO: get settings, add url, commit settings
-
-                callbackContext.success("success: addUrl " + url);
-
-                // TODO: remove vibrator for production
-                Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(500);
-
-                return true;
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try{
+                    if ("addUrl".equals(action)) {
+                        addUrl(args);
+                        command.success();
+                    } else if ("removeUrl".equals(action)) {
+                        removeUrl(args);
+                        command.success();
+                    } else if ("deactivate".equals(action)) {
+                        deactivate(args);
+                        command.success();
+                    } else if ("setInterval".equals(action)) {
+                        setInterval(args);
+                        command.success();
+                    }
+                }
+                catch(Exception e){
+                    command.error(e.getMessage());
+                }
             }
+        });
+
+        return true;
+    }
 
 
-            if ("removeUrl".equals(action)) {
+    private void addUrl(JSONArray args) throws JSONException {
 
-                String url = args.getString(0);
+        String url = args.getString(0);
 
-                // TODO: get settings, remove url, commit settings
-
-                callbackContext.success("success: removeUrl " + url);
-
-                // TODO: remove vibrator for production
-                Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(500);
-
-                return true;
-
-            }
+        // TODO: get settings, add url, commit settings
 
 
-            if ("deactivate".equals(action)) {
+        // TODO: remove vibrator for production
+        Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
 
-                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
-                SharedPreferences.Editor editor = settings.edit();
-                // writes the PollingDate to the settings
-                editor.putLong("PollingPlugin.PollingInterval", 0);
-                editor.commit();
+    private void removeUrl(JSONArray args) throws JSONException {
 
-                AlarmManager alarmMgr = (AlarmManager)(this.cordova.getActivity().getSystemService(Context.ALARM_SERVICE));
+        String url = args.getString(0);
 
-                PendingIntent alarmIntent;
-                Intent intent = new Intent(this.cordova.getActivity(), PollingReceiver.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), 0, intent, 0);
-                alarmMgr.cancel(alarmIntent);
-
-
-                callbackContext.success("success: deactivate");
-
-                // TODO: remove vibrator for production
-                Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(500);
-
-
-                /**********************************************************/
+        // TODO: get settings, add url, commit settings
 
 
 
-                /**********************************************************/
 
-                return true;
-            }
+        // TODO: remove vibrator for production
+        Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
 
 
+    private void deactivate(JSONArray args) throws JSONException {
 
-			if ("setInterval".equals(action)) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
+        SharedPreferences.Editor editor = settings.edit();
+        // writes the PollingDate to the settings
+        editor.putLong("PollingPlugin.PollingInterval", 0);
+        editor.commit();
 
-                long interval = args.getLong(0);
+        AlarmManager alarmMgr = (AlarmManager)(this.cordova.getActivity().getSystemService(Context.ALARM_SERVICE));
 
-                // TODO: uncomment this for production
+        PendingIntent alarmIntent;
+        Intent intent = new Intent(this.cordova.getActivity(), PollingReceiver.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), 0, intent, 0);
+        alarmMgr.cancel(alarmIntent);
+
+        // TODO: remove vibrator for production
+        Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
+
+
+    private void setInterval(JSONArray args) throws JSONException {
+
+        long interval = args.getLong(0);
+
+        // TODO: uncomment this for production
 //				if(interval < MIN_INTERVAL) {
 //					callbackContext.error("The interval must be at least " + MIN_INTERVAL);
 //					return true;
 //				}
 
-                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
-				SharedPreferences.Editor editor = settings.edit();
-				// writes the PollingDate to the settings
-				editor.putLong("PollingPlugin.PollingInterval", interval);
-				editor.commit();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
+        SharedPreferences.Editor editor = settings.edit();
+        // writes the PollingDate to the settings
+        editor.putLong("PollingPlugin.PollingInterval", interval);
+        editor.commit();
 
-				AlarmManager alarmMgr = (AlarmManager)(this.cordova.getActivity().getSystemService(Context.ALARM_SERVICE));
+        AlarmManager alarmMgr = (AlarmManager)(this.cordova.getActivity().getSystemService(Context.ALARM_SERVICE));
 
-                // Create the intent, which will be triggered when the alarm goes off
-                PendingIntent alarmIntent;
-                Intent intent = new Intent(this.cordova.getActivity(), PollingReceiver.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), 0, intent, 0);
-				alarmMgr.cancel(alarmIntent);
-
-
-                // power saving mode
-				// alarm interval: (see https://developer.android.com/training/scheduling/alarms.html)
-                alarmMgr.setInexactRepeating(
-                        AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                        interval /* time until first trigger in millis */,
-                        interval /* interval to trigger again in millis */,
-                        alarmIntent);
-                // exact mode:
-                // TODO: exact alarm scheduling needs one-time alarms that reschedule a new one-time alarm when fired.
+        // Create the intent, which will be triggered when the alarm goes off
+        PendingIntent alarmIntent;
+        Intent intent = new Intent(this.cordova.getActivity(), PollingReceiver.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), 0, intent, 0);
+        alarmMgr.cancel(alarmIntent);
 
 
-                callbackContext.success("success: setInterval " + interval + "ms");
+        // power saving mode
+        // alarm interval: (see https://developer.android.com/training/scheduling/alarms.html)
+        alarmMgr.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                interval /* time until first trigger in millis */,
+                interval /* interval to trigger again in millis */,
+                alarmIntent);
+        // exact mode:
+        // TODO: exact alarm scheduling needs one-time alarms that reschedule a new one-time alarm when fired.
 
-                // TODO: remove vibrator for production
-                Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(500);
 
-				return true;
-			}
-			return false;
-		} catch(Exception e) {
-			System.err.println("Exception: " + e.getMessage());
-			callbackContext.error(e.getMessage());
-			return false;
-		}
-	}
+        // TODO: remove vibrator for production
+        Vibrator v = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
 }
--- src/android/ch/maenu/pollingplugin/PollingReceiver.java
+++ src/android/ch/maenu/pollingplugin/PollingReceiver.java
@@ -1,27 +1,19 @@
 package ch.maenu.pollingplugin;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Vibrator;
 import android.util.Log;
 
-
-
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import ch.unisport.R;
 
 
 public class PollingReceiver extends BroadcastReceiver {
-
-
-    public static final String TITLE = "ALARM_TITLE";
-    public static final String SUBTITLE = "ALARM_SUBTITLE";
-    public static final String TICKER_TEXT = "ALARM_TICKER";
-    public static final String NOTIFICATION_ID = "NOTIFICATION_ID";
-    public static final String ICON = "ALARM_ICON";
-
 
 
 
@@ -29,54 +21,36 @@ public class PollingReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d("PollingPlugin", "PollingEventReceived");
 
-
-//        // removes lock screen and activates screen
-//        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-//        WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-//        wakeLock.acquire();
-//
-//        KeyguardManager keyguardManager = (KeyguardManager)context.getSystemService(Context.KEYGUARD_SERVICE);
-//        KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
-//        keyguardLock.disableKeyguard();
+        // bitmap http://stackoverflow.com/questions/11182714/bitmapfactory-example
 
 
+        Bitmap bMap = BitmapFactory.decodeResource(context.getResources(), R.drawable.screen);
 
-        // TODO: remove vibrator for production
+        Notification noti = new Notification.Builder(context)
+                .setContentTitle("Kurs XYZ")
+                .setContentText("Du kannst dich nun anmelden!")
+                .setSmallIcon(R.drawable.icon)
+                .setLargeIcon(bMap)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        notificationManager.notify(9999, noti);
+
+
+
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(1000);
+        int dot = 300;
+        int short_gap = 200;
+        long[] pattern = {
+                0,  // Start immediately
+                300, 200, 300
+        };
+
+        // Only perform this pattern one time (-1 means "do not repeat")
+        v.vibrate(pattern, -1);
+
+
 //        performAction(context);
-
-
-
-       /*#######################################################################*/
-        /**
-         * NOTIFICATION
-         *
-         * http://www.vogella.com/tutorials/AndroidNotifications/article.html
-         */
-       /*#######################################################################*/
-
-
-//        // Prepare intent which is triggered if the
-//        // notification is selected
-////        Intent intent = new Intent(this, NotificationReceiverActivity.class);
-//
-//        PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
-//
-//        // Build notification
-//        // Actions are just fake
-//        Notification notification = new Notification.Builder(context)
-//                .setContentTitle("USP test notification!")
-//                .setContentIntent(pIntent).build();
-//
-//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        // hide the notification after its selected
-//        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-//
-//        notificationManager.notify(0, notification);
-
-
-       /*#######################################################################*/
 
 
 
